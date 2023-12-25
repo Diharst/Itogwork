6ebd10a57819fd15d1083ededc96a403b6e43321
Dmitriy <kravchenkodv03@gmail.com>
2023-12-26 00:32:02 +0300

    создали новый проект и написал код

---
+++ itog/Program.cs
@@ -0,0 +1,46 @@
+﻿int ReadInt(string size)
+{
+    System.Console.WriteLine(size);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+string[] GenerateMatrix(int a)
+{
+string[] matrix = new string[a];
+Random rand = new Random();
+
+for (int i = 0; i < a; i++)
+{
+        System.Console.Write("Введите элемент массива: ");
+        matrix[i] = Console.ReadLine();
+    
+}
+return matrix;
+}
+
+void PrintMatrix(string[] matrix)
+{
+System.Console.Write("[" + string.Join(", ", matrix) + "]");
+}
+
+string[] Find(string[] matrix)
+{
+    int count = 0;
+    string[] massiv = new string[matrix.Length];
+    for(int i = 0; i < matrix.Length; i++)
+    {
+        if(matrix[i].Length<=3)
+        massiv[i] = matrix[count];
+        count++;
+    }
+    return massiv;
+}
+
+
+int size = ReadInt("Введите размер массива");
+string[] massiv = GenerateMatrix(size);
+PrintMatrix(massiv);
+string[] massiv1 = Find(massiv);
+massiv1 = massiv1.Where(i => i != null).ToArray();
+System.Console.Write(" -> ");
+System.Console.WriteLine("[" + string.Join(", ", massiv1) + "]");
